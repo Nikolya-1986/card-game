d2dcae04c6bdc8face0cc0207b1bbb10322eb48e
NIKOLAI <nikolai_sergeevich@mail.ru>
2022-08-04 13:37:47 +0300

    create game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameCardComponent } from './components/game-card/game-card.component';
+import { RestartDialogComponent } from './components/restart-dialog/restart-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameCardComponent,
+    RestartDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
